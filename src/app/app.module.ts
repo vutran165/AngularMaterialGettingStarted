d87dd392436fa5c10b388f83f0135b1d4a8c6211
vutran165 <vutran165@gmail.comm>
2018-05-31 15:34:30 +0700

    initialize

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    FormsModule,
+    MaterialModule,
+    BrowserAnimationsModule ,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material/material.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatMenuModule,
+  MatToolbarModule, MatIconModule, MatCardModule} from '@angular/material';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule, MatIconModule, MatCardModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule
+    ],
+  declarations: [],
+})
+export class MaterialModule { }

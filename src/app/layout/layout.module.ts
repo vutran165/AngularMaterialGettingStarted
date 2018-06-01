7706e4dcbfc6197e91a6d86a0779b53ee6adf650
vutran165 <vutran165@gmail.com>
2018-06-01 22:55:11 +0700

    01062018-v3

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,14 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from '../layout/layout.component';
-import { ComponentComponent } from './component/component/component.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { HeaderComponent } from './components/header/header.component';
+import { SidebarComponent } from '../layout/components/sidebar/sidebar.component';
+import { HeaderComponent } from '../layout/components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [LayoutComponent, ComponentComponent, SidebarComponent, HeaderComponent]
+  declarations: [LayoutComponent, SidebarComponent, HeaderComponent]
 })
 export class LayoutModule { }

445bdb8279965390fbda28bffeadf8f706906165
vutran165 <vutran165@gmail.comm>
2018-06-07 17:25:38 +0700

    07062018

--- src/app/layout/components/sidebar/sidebar.component.ts
+++ src/app/layout/components/sidebar/sidebar.component.ts
@@ -1,4 +1,51 @@
 import { Component, OnInit } from '@angular/core';
+import {MatTreeNestedDataSource} from '@angular/material/tree';
+import { NestedTreeControl } from '@angular/cdk/tree';
+import { BehaviorSubject, of as observableOf } from 'rxjs';
+
+// FileNode...
+export class FileNode {
+  children: FileNode[];
+  filename: string;
+  type: any;
+}
+
+const TREE_DATA = `
+  {
+    "Documents": {
+      "angular": {
+        "src": {
+          "core": "ts",
+          "compiler": "ts"
+        }
+      },
+      "material2": {
+        "src": {
+          "button": "ts",
+          "checkbox": "ts",
+          "input": "ts"
+        }
+      }
+    },
+    "Downloads": {
+        "Tutorial": "html",
+        "November": "pdf",
+        "October": "pdf"
+    },
+    "Pictures": {
+        "Sun": "png",
+        "Woods": "jpg",
+        "Photo Booth Library": {
+          "Contents": "dir",
+          "Pictures": "dir"
+        }
+    },
+    "Applications": {
+        "Chrome": "app",
+        "Calendar": "app",
+        "Webstorm": "app"
+    }
+  }`;
 
 
 @Component({
